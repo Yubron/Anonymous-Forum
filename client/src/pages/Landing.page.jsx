b4aeb68a66d6883916876324a4c5da69a4728e5a
Yubron <wlstn4966@gmail.com>
2022-03-26 01:15:12 +0900

    front: add no data

--- client/src/pages/Landing.page.jsx
+++ client/src/pages/Landing.page.jsx
@@ -16,25 +16,27 @@ const LandingPage = () => {
   return (
     <MainContainer>
       <Title> Board List </Title>
-      <Table>
-        <TR>
-          <TD> No </TD>
-          <TD> 타이틀 </TD>
-          <TD> 작성자 </TD>
-        </TR>
-        {
-          data.data.map(board => {
-            return (
-              <TR>
-                <TD style={{width: '50px'}}> {board.id}</TD>
-                <TD style={{width: '400px', cursor: 'pointer'}} onClick={() => navigate(`/${board.id}`)}> {board.title}</TD>
-                <TD style={{width: '100px'}}> {board.writer}</TD>
-              </TR>
-            )
-          })
-        }
-      </Table> 
-
+      {data.data.length !== 0 ? 
+        <Table>
+          <TR>
+            <TD> No </TD>
+            <TD> 타이틀 </TD>
+            <TD> 작성자 </TD>
+          </TR>
+          {
+            data.data.map(board => {
+              return (
+                <TR>
+                  <TD style={{width: '50px'}}> {board.id}</TD>
+                  <TD style={{width: '400px', cursor: 'pointer'}} onClick={() => navigate(`/${board.id}`)}> {board.title}</TD>
+                  <TD style={{width: '100px'}}> {board.writer}</TD>
+                </TR>
+              )
+            })
+          }
+        </Table> 
+      : <h2> NO DATA </h2> 
+      }
       <WriteButton onClick={() => navigate('/create')}> 등록 </WriteButton>
     </MainContainer>
   )
